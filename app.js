6ca1c765d15b359d2f7e8a9f2056b1868d7d84f3
willianwt2000 <willianmetelien@gmail.com>
2025-01-24 20:36:32 -0400

    adding a newRamaWt

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const button = document.querySelector('button');
 button.addEventListener('click', () => {
   alert('Hola mundo');
+
+  let variable = 'Hola mundo';
+  console.log(variable);
+
+  console.log("Rama '  Nueva Rama wtihout conflict'");
 });
